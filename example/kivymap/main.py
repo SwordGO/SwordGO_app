6d51c1957959f91c54667e95479cf762ed589fda
borosnborea <boros@daum.net>
2016-11-20 16:49:17 +0900

    app with api

--- example/kivymap/main.py
+++ example/kivymap/main.py
@@ -9,13 +9,50 @@ from kivy.lang import Builder
 from kivy.uix.bubble import Bubble
 from kivy.clock import Clock, mainthread
 from plyer import gps
-
-
+import requests
+url = "http://build.ees.guru:8888"
+api_url = url+"/mines"
+markerlist = []
 class MapMark(MapMarkerPopup):
+    def remove_open_status(self):
+        if self.is_open and self.placeholder.parent:
+            self.is_open = not self.is_open
+            super(MapMarkerPopup, self).remove_widget(self.placeholder)
     pass
-    
-marker_loader = '''
+
+class MapViewer(MapView):
+    def on_touch_down(self, touch):
+        self.deletemark()
+        return super(MapViewer, self).on_touch_down(touch)
+
+
+    def deletemark(self):
+        global markerlist
+        for i in markerlist:
+            i.remove_open_status()
+        
+    pass
+marker_loader_ = '''
 MapMarkerPopup:
+    lat: %(lat)f
+    lon: %(lon)f
+    source: "icons/marker%(source)d.png"
+    popup_size: dp(230), dp(130)
+
+    Bubble:
+        BoxLayout:
+            orientation: "horizontal"
+            padding: "5dp"
+            AsyncImage:
+                source: "http://upload.wikimedia.org/wikipedia/commons/9/9d/France-Lille-VieilleBourse-FacadeGrandPlace.jpg"
+                mipmap: True
+            Label:
+                text: "[b]Lille[/b]\\n1 154 861 hab\\n5 759 hab./km2"
+                markup: True
+                halign: "center"
+'''
+marker_loader = '''
+MapMark:
     lat: %(lat)f
     lon: %(lon)f
     source: "icons/marker%(source)d.png"
@@ -41,7 +78,7 @@ class MapViewApp(App):
     def build(self):
         gps.configure(on_location=self.on_location,on_status=self.on_status)
         #self.root.add_widget(Builder.load_file('buttons.kv'))
-        self.mapview = MapView(zoom=15, lat=37.4251096, lon=127.4250295)
+        self.mapview = MapViewer(zoom=15, lat=37.4251096, lon=127.4250295)
         #self.mapview = MapView(zoom=15, lat=50.6494,lon=3.05)
         #gps.start(1000, 0)
         b=MapMark(lat=50.6494,lon=3.057,popup_size= (dp(230), dp(130)),source = "icons/marker2.png")
@@ -72,5 +109,17 @@ class MapViewApp(App):
         self.gps_status = 'type={}\n{}'.format(stype, status)
     
     def makemark(self):
-        self.mapview.add_widget(MapMark(lat=self.mapview.lat,lon=self.mapview.lon,popup_size= (dp(230), dp(130)),source ="icons/marker1.png" ))
+        response = requests.post(api_url+"/%(lat)f/%(lon)f/test"%{'lat':self.mapview.lat,'lon':self.mapview.lon},"")        
+        status = response.json()['state']
+        if status == "FAIL" : 
+            return
+        response = requests.get(api_url,"")
+        for i in response.json():
+            global markerlist
+            #print "id = %s\tlat,lon = %s,%s\tHP = %s"%(l['idmine'],l['lat'],l['lon'],l['hp'])
+            _widget = Builder.load_string(marker_loader%{"lat":i['lat'],"lon":i['lon'],"source":2})
+            self.mapview.add_widget(_widget)
+            markerlist.append(_widget)
+            #self.mapview.add_widget(MapMark(lat=i['lat'],lon=i['lon'] ,   popup_size= (dp(230), dp(130)),source ="icons/marker1.png" ))
+        
 MapViewApp().run()
