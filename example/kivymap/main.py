efe53caa1e410fd5e235695001dd4471c5928496
borosnborea <boros@daum.net>
2016-11-22 17:41:29 +0900

    WTF -_-

--- example/kivymap/main.py
+++ example/kivymap/main.py
@@ -19,7 +19,7 @@ url = "http://build.ees.guru:8888"
 api_url = url+"/mines"
 user_url = url+"/user"
 inven_url = url+"/inven"
-markerlist = []
+markerdic = {}
 GL_USER = 1
 class MapMark(MapMarkerPopup):
     def remove_open_status(self):
@@ -34,11 +34,10 @@ class MapViewer(MapView):
         return super(MapViewer, self).on_touch_down(touch)
 
 
-    def deletemark(self):
-        global markerlist
-        for i in markerlist:
+    def deletemark(self):        
+        global markerdic        
+        for i in markerdic.values():
             i.remove_open_status()
-        
     pass
 whereami_loader = '''
 MapMarker:
@@ -105,11 +104,11 @@ class MapViewApp(App):
             self.root = MapViewBackground()
             gps.configure(on_location=self.on_location,on_status=self.on_status)
             self.mapview = self.root.ids.mv
-            response = requests.get(api_url,"")        
-
+            response = requests.get(api_url+"/%(lat)f/%(lon)f?around=1000"%{'lat':self.mapview.lat,'lon':self.mapview.lon},"")                      
                 
             for i in response.json():
-                global markerlist
+                
+                global markerdic
                 #print "id = %s\tlat,lon = %s,%s\tHP = %s"%(l['idmine'],l['lat'],l['lon'],l['hp'])
                 user_id = requests.get(user_url+"/%(user)s/test"%i,"")
                 user_id = user_id.json()
@@ -120,13 +119,14 @@ class MapViewApp(App):
                 
                 _widget = Builder.load_string(marker_loader%{"lat":i['lat'],"lon":i['lon'],"source":user_id['team'],"user_name":user_id['name'], "team_name":user_id['team_name']})
                 self.mapview.add_widget(_widget)
-                markerlist.append(_widget)
+                
+                markerdic[i['idmine']] = _widget
             inven_respone = requests.get(inven_url+"/%d/test"%GL_USER,"")
             self.mineral_cobble = inven_respone.json()[0]['cob']
             self.mineral_col = inven_respone.json()[0]['col']
             self.mineral_steel = inven_respone.json()[0]['steel']
             self.mineral_gold = inven_respone.json()[0]['gold']
-            self.start(1000,0)
+            self.start(10000,0)
         except Exception as e:        
             self.error = "                             "+str(e)
         return self.root
@@ -140,20 +140,41 @@ class MapViewApp(App):
     beacon = None
     @mainthread
     def on_location(self, **kwargs):
-    
-        if self.beacon: self.beacon.detach()
-        self.gps_location = '\n'.join(['{}={}'.format(k, v) for k, v in kwargs.items()])        
-        #self.mapview = MapView(zoom=15, lat=kwargs.get('lat'), lon=kwargs.get('lon'))
-        self.mapview.center_on(kwargs.get('lat'),kwargs.get('lon'))
-        self.beacon = Builder.load_string(whereami_loader%{"lat":kwargs.get('lat'),"lon":kwargs.get('lon')})
-        self.mapview.add_widget(self.beacon)
-        #self.mapview = MapView(zoom=15, lat=37.3251096, lon=127.1250295)
+        try:
+            if self.beacon: self.beacon.detach()
+            self.gps_location = '\n'.join(['{}={}'.format(k, v) for k, v in kwargs.items()])        
+            #self.mapview = MapView(zoom=15, lat=kwargs.get('lat'), lon=kwargs.get('lon'))
+            self.mapview.center_on(kwargs.get('lat'),kwargs.get('lon'))
+            self.beacon = Builder.load_string(whereami_loader%{"lat":kwargs.get('lat'),"lon":kwargs.get('lon')})
+            self.mapview.add_widget(self.beacon)
+            #self.mapview = MapView(zoom=15, lat=37.3251096, lon=127.1250295)
+            response = requests.get(api_url+"/%(lat)f/%(lon)f?around=100"%{'lat':self.mapview.lat,'lon':self.mapview.lon},"")                      
+            for i in response.json():
+                
+                global markerdic
+                if i['idmine'] in markerdic:
+                #TODO UPDATE
+                else:
+                #print "id = %s\tlat,lon = %s,%s\tHP = %s"%(l['idmine'],l['lat'],l['lon'],l['hp'])
+                user_id = requests.get(user_url+"/%(user)s/test"%i,"")
+                user_id = user_id.json()
+                #status2 = user_id.json()['state']
+                #if status2 == "FAIL" : 
+                #    return
+                
+                
+                _widget = Builder.load_string(marker_loader%{"lat":i['lat'],"lon":i['lon'],"source":user_id['team'],"user_name":user_id['name'], "team_name":user_id['team_name']})
+                self.mapview.add_widget(_widget)
+                
+                markerdic[i['idmine']] = _widget
         
+        except Exception as e:        
+            self.error = "                             "+str(e)
     @mainthread
     def on_status(self, stype, status):
         self.gps_status = 'type={}\n{}'.format(stype, status)
         
-    
+    '''
     def makemark(self):
         try:
             response = requests.post(api_url+"/%(lat)f/%(lon)f/test"%{'lat':self.mapview.lat,'lon':self.mapview.lon},"")        
@@ -163,7 +184,7 @@ class MapViewApp(App):
             response = requests.get(api_url,"")
             
             for i in response.json():
-                global markerlist
+                global markerlist                
                 #print "id = %s\tlat,lon = %s,%s\tHP = %s"%(l['idmine'],l['lat'],l['lon'],l['hp'])
                 user_id = requests.get(user_url+"/%(user)s/test"%i,"")
                 user_id = user_id.json()
@@ -175,26 +196,32 @@ class MapViewApp(App):
                 _widget = Builder.load_string(marker_loader%{"lat":i['lat'],"lon":i['lon'],"source":user_id['team'],"user_name":user_id['name'], "team_name":user_id['team_name']})
                 self.mapview.add_widget(_widget)
                 markerlist.append(_widget)
+                
                 #self.mapview.add_widget(MapMark(lat=i['lat'],lon=i['lon'] ,   popup_size= (dp(230), dp(130)),source ="icons/marker1.png" ))
         except Exception as e:        
             self.error = "                     "+str(e)
-            
+    no more use ... 
+    '''
+    
     def makemark2(self):
         try:
             response = requests.get(api_url+"/%(lat)f/%(lon)f?around=50"%{'lat':self.mapview.lat,'lon':self.mapview.lon},"")
             response = response.json()
             if not response:
-                response = requests.post(api_url+"/%(lat)f/%(lon)f/test"%{'lat':self.mapview.lat,'lon':self.mapview.lon},"")        
-                status = response.json()['state']
+                response2 = requests.post(api_url+"/%(lat)f/%(lon)f/test"%{'lat':self.mapview.lat,'lon':self.mapview.lon},"")        
+                status = response2.json()['state']
                 if status == "FAIL" : 
                     return
                 user_id = requests.get(user_url+"/%d/test"%GL_USER,"")
                 user_id = user_id.json()
                 _widget = Builder.load_string(marker_loader%{"lat":self.mapview.lat,"lon":self.mapview.lon,"source":user_id['team'],"user_name":user_id['name'], "team_name":user_id['team_name']})
                 self.mapview.add_widget(_widget)
-                markerlist.append(_widget)
+                
+                
+                markerdic[response2.json()['id']] = _widget
             else :
                 self.root.popup.open()
         except Exception as e:        
                 self.error = "                             "+str(e)
+                
 MapViewApp().run()
